b227f17b45214280f5bcd41eda9a1b38f6b3fb30
Eugene Theriault <hybridbassist@gmail.com>
2020-11-13 16:48:55 -0800

    Create unicorn

---
+++ lib/unicorn.py
@@ -0,0 +1,10 @@
+class Unicorn:
+    def __init__(self, name, color = "white"):
+        self.name = name
+        self.color = color
+
+    def is_white(self):
+        return self.color == "white"
+
+    def say(self, string):
+        return f"**;* {string} **;*"
